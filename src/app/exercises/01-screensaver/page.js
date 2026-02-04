d981874772dce14b3379d9053d8dda8d51fa765e
IYarosh <iaros1298@gmail.com>
2026-02-04 14:22:54 +0300

    Excercise 1

--- src/app/exercises/01-screensaver/page.js
+++ src/app/exercises/01-screensaver/page.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
 import ScreenSaver from '../../../components/ScreenSaver';
+import { COLORS } from './namedColors';
+import Link from 'next/link';
 
 function ScreenSaverExercise() {
   return (
-    <main className="screen-saver-wrapper">
-      <ScreenSaver color="red" />
+    <main>
+      <ol>
+        {COLORS.map(color => (
+          <li>
+            <Link href={`/exercises/01-screensaver/${color}`}>{color}</Link>
+          </li>
+        ))}
+      </ol>
     </main>
   );
 }
